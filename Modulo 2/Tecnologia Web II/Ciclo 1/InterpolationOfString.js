5eb15c7cc8bedceedf867903c0fc5a953494fa3f
Daniel Dantas <contatolobinhodev@gmail.com>
2022-03-14 09:04:57 -0300

    Create InterpolationOfString.js

---
+++ Modulo 2/Tecnologia Web II/Ciclo 1/InterpolationOfString.js
@@ -0,0 +1,7 @@
+let user = { 
+  firstName: 'Hcode', 
+  lastName: 'Treinamentos', 
+  city: 'Rio de Janeiro' 
+}
+
+console.log(`Olá, meu nome ${user.firstName} ${user.lastName}. Sou de ${user.city}`)
