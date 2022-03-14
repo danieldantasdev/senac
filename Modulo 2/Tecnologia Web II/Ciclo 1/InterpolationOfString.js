1d4f29a9f647fd62e4f43ef8819958b9a0197a87
Daniel Dantas <contatolobinhodev@gmail.com>
2022-03-14 09:31:29 -0300

    Update InterpolationOfString.js

--- Modulo 2/Tecnologia Web II/Ciclo 1/InterpolationOfString.js
+++ Modulo 2/Tecnologia Web II/Ciclo 1/InterpolationOfString.js
@@ -1,7 +1,24 @@
 let user = { 
-  firstName: 'Hcode', 
-  lastName: 'Treinamentos', 
+  firstName: 'Daniel', 
+  lastName: 'Dantas', 
   city: 'Rio de Janeiro' 
 }
 
 console.log(`Olá, meu nome ${user.firstName} ${user.lastName}. Sou de ${user.city}`)
+
+let table = document.createElement('table'); table.border=1;
+table.innerHTML = `<thead>
+                      <tr>                                                               
+                        <th>Nome</th>
+                        <th>Sobremenome</th>
+                        <th>Cidade</th>                        
+                       </tr>   
+                    </thead>  
+                    
+                    <tbody>
+                        <tr>
+                          <td>${user.firstName} </td> 
+                          <td>${user.lastName} </td>
+                          <td>${user.city}</td></tr>
+                     </tbody>`; 
+document.querySelector('body').append(table)
