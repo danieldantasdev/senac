a849665aa802dab4f80ed0ff48dfb7c6f7f84c0c
lobinhodev <contatolobinhodev@gmail.com>
2022-03-15 00:40:18 -0300

    .

--- Modulo 2/Estrutura de dados/c/comparaString2.cpp
+++ Modulo 2/Estrutura de dados/c/comparaString2.cpp
@@ -1,48 +1,5 @@
-// // Online C compiler to run C program online
-// // #include <stdio.h>
-
-// // int main()
-// // {
-// //     // Write C code here
-// //     int index = 0;
-
-// //     // Enquanto os caracteres forem iguais, corre os vectores.
-// //     while (a[index] == b[index])
-// //     {
-// //         // Verifica se alguma das strings terminou.
-// //         // Se sim, sai do loop. Caso contrario, analisa o próximo carácter.
-// //         if (a[index] == '\0' || b[index] == '\0')
-// //             break;
-// //         ++index;
-// //     }
-
-// //     // Se ambos terminaram, as strings contidas são iguais.
-// //     if (a[index] == '\0' && b[index] == '\0')
-// //         return 0;
-// //     else
-// //         return -1;
-// // }
-
 #include <stdio.h>
 #include <stdlib.h>
-
-// // int main()
-// // {
-
-// //     char vet1[20] = "Bom ";
-// //     char vet2[20] = "dia";
-
-// //     for (int i = 0; vet1[i] != '\0'; i++)
-// //     {
-// //         for (int j = 0; vet2[j] != '\0'; j++)
-// //         {
-// //             vet1[i] = vet2[j];
-// //             printf("%c%c", vet1[i], vet2[j]);
-// //         }
-// //     }
-
-// //     return 0;
-// // }
 
 int main()
 {
@@ -52,14 +9,16 @@ int main()
     char vet2[20];
     int i, j = 0;
 
-    printf("Segunda frase: ");
+    printf("Primeira frase: ");
     fgets(vet1, 50, stdin);
 
-    printf("Primeira frase: ");
+    printf("Segunda frase: ");
     fgets(vet2, 50, stdin);
-    for (i = 0; vet1[i] != '\n'; i++)
+
+    for (i = 0; vet1[i] != '\0'; i++)
         ;
-    for (j = 0; vet2[j] != '\n'; j++)
+
+    for (j = 0; vet2[j] != '\0'; j++)
         vet1[i++] = vet2[j];
     printf("%s", vet1);
 }
@@ -92,4 +51,64 @@ int main()
 //         }
 //     }
 //     printf("%s", str3);
+// }
+
+// int main()
+// {
+
+//     char str1[51];
+//     char str2[51];
+//     char str3[51];
+//     int i, j;
+
+//     printf("Digite aqui uma palavra: ");
+//     fgets(str1, 51, stdin);
+//     printf("Digite aqui outra palavra: ");
+//     fgets(str2, 51, stdin);
+
+//     for (i = 0; str1[i] != '\n'; i++)
+//     {
+//     }
+
+//     for (j = 0; str2[j] != '\n'; j++, i++)
+//     {
+//         str1[i] = str2[j];
+//     }
+
+//     printf("%s", str1);
+// }
+
+// int main()
+// {
+
+//     char str1[51];
+//     char str2[51];
+//     char str3[51];
+//     int i, j;
+
+//     printf("Digite aqui uma palavra: ");
+//     fgets(str1, 51, stdin);
+//     printf("Digite aqui outra palavra: ");
+//     fgets(str2, 51, stdin);
+
+//     for (i = 0; str1[i] != '\0'; i++)
+//     {
+//         str3[i] = str1[i];
+//         if (str3[i] == '\n')
+//         {
+//             str3[i] = ' ';
+//             ++i;
+//             for (j = 0; str2[j] != '\0'; j++)
+//             {
+//                 str3[i] = str2[j];
+//                 ++i;
+//             }
+//         }
+//     }
+//     for (i = 0; str3[i] != '\0'; i++)
+//     {
+//         str1[i] = str3[i];
+//     }
+
+//     printf("%s", str1);
 // }
