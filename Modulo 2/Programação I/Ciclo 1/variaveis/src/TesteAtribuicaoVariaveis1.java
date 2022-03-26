6d1e3b28cfd4dc834e8bf42069b825d3f7169570
Daniel Dantas <contatolobinhodev@gmail.com>
2022-03-25 21:43:52 -0300

    Add files via upload

---
+++ Modulo 2/Programação I/Ciclo 1/variaveis/src/TesteAtribuicaoVariaveis1.java
@@ -0,0 +1,38 @@
+
+public class TesteAtribuicaoVariaveis1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Definição de vareiáveis
+		boolean b;
+		char c;
+		byte by;
+		int i;
+		short s;
+		long l;
+		float f;
+		double d;
+
+		//Atribuição de variáveis
+		b = true;
+		c = '1';
+		by = 0;
+		i = 0;
+		s = 0;
+		l = 0;
+		f = 0;
+		d = 0;
+		
+		//Exibição dos valores
+		System.out.println("b="+b);
+		System.out.println("b="+c);
+		System.out.println("b="+by);
+		System.out.println("b="+i);
+		System.out.println("b="+s);
+		System.out.println("b="+l);
+		System.out.println("b="+f);
+		System.out.println("b="+d);
+
+	}
+
+}
