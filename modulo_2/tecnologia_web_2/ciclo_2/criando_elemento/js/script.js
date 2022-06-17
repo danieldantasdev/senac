ac651438e087911657e6fb1da1594723ef50f01e
lobinhodev <contatolobinhodev@gmail.com>
2022-06-17 16:56:38 -0300

    .

---
+++ modulo_2/tecnologia_web_2/ciclo_2/criando_elemento/js/script.js
@@ -0,0 +1,25 @@
+var lista = document.getElementById('lista')
+
+var item = document.querySelector('#lista li')
+
+item.style.border = '2px solid blue'
+
+const inserirItem = () => {
+    const numeroIncrementado = parseInt(lista.childElementCount) + 1
+
+    var item = document.createElement('li')
+    item.innerHTML = `item ${numeroIncrementado}`
+    lista.appendChild(item)
+}
+
+const removerItem = () => {
+    lista.removeChild(lista.lastChild)
+}
+
+const mouseOver = () => {
+    item.style.backgroundColor = 'red'
+}
+
+const mouseOut = () => {
+    item.style.backgroundColor = 'transparent'
+}
