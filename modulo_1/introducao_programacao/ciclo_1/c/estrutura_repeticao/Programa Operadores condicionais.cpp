5d8fbe442e0e07c905fc3b393ce67a61fde40222
lobinhodev <contatolobinhodev@gmail.com>
2022-06-19 18:06:44 -0300

    🌈 style:acertando as branchs

--- modulo_1/introducao_programacao/ciclo_1/c/estrutura_repeticao/Programa Operadores condicionais.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main (){
-	
-	float A = 4, B = -2, C = 1, TESTE = 1;
-	
-	A + B == 0 && C != 1;
-	(TESTE == 1) || A * C > B;
-}
--- modulo_2/estrutura_dados/ciclo_1/string/string.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    char string[100];
-    printf("Digite seu nome: ");
-    fgets(string, 100, stdin); // gets (string)
-    printf("\n Ola %s", string);
-}
