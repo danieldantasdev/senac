022eb0394f1c7d1cc2c8500d73e31d23b71d0cf2
lobinhodev <contatolobinhodev@gmail.com>
2022-06-23 10:54:16 -0300

    ✨ feat:implementando as funcionalidade de adicionar elementos no array

--- modulo_2/tecnologia_web_2/ciclo_2/criando_elemento/js/script.js
+++ modulo_2/tecnologia_web_2/ciclo_2/criando_elemento/js/script.js
@@ -26,3 +26,56 @@ const mouseOver = () => {
 const mouseOut = () => {
     item.style.backgroundColor = 'transparent'
 }
+
+/* Form */
+const imprimeValor = () => {
+    var campoNome = document.getElementById('nome')
+    alert(`Seu nome é ${campoNome.value}`)
+}
+
+const novoItem = () => {
+    var camposForm = document.getElementsByTagName('input')
+
+    var novaLinha = document.createElement('tr')
+
+    for (var i = 0; i < camposForm.length; i++) {
+        var novaColuna = document.createElement('td')
+
+        novaColuna.innerHTML = camposForm[i].value
+
+        novaLinha.appendChild(novaColuna)
+    }
+
+    var indice = itens.options[itens.selectedIndex].index
+
+    var itemSelecionado = itens.options[itens.selectedIndex].text
+
+    var selecao = document.createElement('td')
+    selecao.innerHTML = itemSelecionado
+    novaLinha.appendChild(selecao)
+
+    // if (camposForm == undefined) {
+    var tabela = document.getElementById('tabela')
+
+    tabela.appendChild(novaLinha)
+    // } else {
+    //     alert('Preencha os campos')
+    // }
+}
+
+const validarFormulario = () => {
+    let nomePessoa = document.forms['formCadastro']['nome'].value
+
+    let emailPessoa = document.forms['formCadastro']['email'].value
+
+    if (nomePessoa == '' || nomePessoa == undefined) {
+        alert('Favor informar seu nome...')
+        event.preventDefault()
+    } else if (emailPessoa == '' || emailPessoa == undefined) {
+        alert('Favor informar seu email...')
+        event.preventDefault()
+    } else {
+        alert(`Olá, ${nomePessoa}, seu email é ${emailPessoa}`)
+        event.preventDefault()
+    }
+}
