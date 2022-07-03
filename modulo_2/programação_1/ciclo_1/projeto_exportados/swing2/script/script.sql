b6c8a5d4ff8665ee17cac4e1adc78efa4e931f00
lobinhodev <contatolobinhodev@gmail.com>
2022-07-03 14:30:49 -0300

    ✨ feat:alterando as queries

--- modulo_2/programação_1/ciclo_1/projeto_exportados/swing2/script/script.sql
+++ modulo_2/programação_1/ciclo_1/projeto_exportados/swing2/script/script.sql
@@ -8,12 +8,12 @@ CREATE TABLE IF NOT EXISTS `funcionario` (
   `nome` varchar(255) NOT NULL,
   `sobrenome` varchar(255) NOT NULL,
   `cargo` varchar(255) NOT NULL,
-  `salario` varchar(255) NOT NULL,
+  `salario` DOUBLE(9,2) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
-ALTER TABLE `funcionario`ADD COLUMN `id_funcionario` INT NOT NULL AFTER `id`;
+-- ALTER TABLE `funcionario`ADD COLUMN `id_funcionario` INT NOT NULL AFTER `id`;
 
-ALTER TABLE `funcionario` MODIFY COLUMN `salario` DOUBLE(9,2) NOT NULL;
+-- ALTER TABLE `funcionario` MODIFY COLUMN `salario` DOUBLE(9,2) NOT NULL;
 
-ALTER TABLE `funcionario` DROP COLUMN `id`;
+-- ALTER TABLE `funcionario` DROP COLUMN `id`;
