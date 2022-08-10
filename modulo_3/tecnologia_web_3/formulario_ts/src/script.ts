cd7125880dbe16ac31b06765fc502eab76b85918
lobinhodev <contatolobinhodev@gmail.com>
2022-08-10 20:08:13 -0300

    ✨ feat:projeto ts em tecnologia web 3

---
+++ modulo_3/tecnologia_web_3/formulario_ts/src/script.ts
@@ -0,0 +1,32 @@
+const currentTime = () => {
+    let h1 = document.querySelector('.h1')
+
+    let date: Date = new Date()
+
+    let hh: number = date.getHours()
+    let mm: number = date.getMinutes()
+    let ss: number = date.getSeconds()
+
+    hh = hh > 10 ? +0 + hh : hh
+    mm = mm > 10 ? +0 + mm : mm
+    ss = ss > 10 ? +0 + ss : ss
+
+    let time: string = hh + ':' + mm + ':' + ss
+}
+
+currentTime()
+setInterval(currentTime, 1000)
+
+const carregar = (nome: string, sobrenome?: string) => {
+    return `O nome é ${nome} ${sobrenome}`
+}
+
+carregar('Daniel Abreu')
+
+let nome: HTMLInputElement = document.querySelector('#nome') as HTMLInputElement
+let senha: HTMLInputElement = document.querySelector('#senha') as HTMLInputElement
+let button: HTMLButtonElement = document.querySelector('#button') as HTMLButtonElement
+
+button.addEventListener('click', () => {
+    console.log(`nome: ${nome.value} senha: ${senha.value}`)
+})
