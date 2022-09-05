f229420a264f0ebdc3e928020c93d5760fc2225d
lobinhodev <contatolobinhodev@gmail.com>
2022-09-05 19:14:06 -0300

    ✨ feat:implementando excessões em Java

---
+++ modulo_3/programacao_2/src/aula_exceptions/TesteException.java
@@ -0,0 +1,32 @@
+package aula_exceptions;
+
+public class TesteException {
+	public static void main(String[] args) {
+		System.out.println("Inicio do metodo main");
+		metodo1();
+		System.out.println("Fim do metodo main");
+	}
+
+	private static void metodo1() {
+		System.out.println("Inicio do metodo 1");
+		metodo2();
+		System.out.println("Fim do metodo 1");
+	}
+
+	private static void metodo2() {
+		System.out.println("Inicio do metodo 2");
+		try {
+			int array[] = new int[10];
+
+			for (int i = 0; i < 20; i++) {
+				array[i] = i;
+				System.out.println(i);
+			}
+		}catch (Exception e) {
+			System.out.println("Erro de estouro de Array: " + e);
+		}
+		
+		System.out.println("Inicio do metodo 2");
+
+	}
+}
